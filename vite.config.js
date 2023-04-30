019aa5aa264b1bc78bace34215d77253cc6ab819
LeandroF01 <leaylio3327@gmail.com>
2023-04-30 13:43:10 -0300

    Add config vite

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+	root: "./src",
+	base: "/",
+	publicDir: "public",
+	build: {
+		outDir: "dist",
+		assetsDir: "assets",
+		rollupOptions: {
+			input: {
+				main: "./main.js", // Ruta al archivo principal de tu aplicación
+			},
+		},
+	},
+	plugins: [],
+});
