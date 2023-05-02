24b0754bab65f4acff5ee8d76fd73514ea83c237
LeandroF01 <leaylio3327@gmail.com>
2023-05-02 10:55:31 -0300

    Add render of cell and logic, with style color board

--- src/components/ChessBoard.js
+++ src/components/ChessBoard.js
@@ -1,3 +1,5 @@
+import "./ChessCell.js";
+
 class ChessBoard extends HTMLElement {
 	constructor() {
 		super();
@@ -10,11 +12,24 @@ class ChessBoard extends HTMLElement {
             --pleace-size: 54px;
             --cell-size: 72px;
 			--board-size: 576px;
+
+			--color-odd: #eed2aa;
+			--color-even: #90502f;
         }
 		.board{
+			display: flex;
+			flex-wrap: wrap;
 			width: var(--board-size);
 			height: var(--board-size);
-			background: #90502f;
+			background: var(--color-even);
+
+			background: conic-gradient(
+				var(--color-even) 90deg,
+				var(--color-odd) 90deg 180deg,
+				var(--color-even) 180deg 270deg,
+				var(--color-odd) 270deg
+			);
+			background-size: calc(var(--cell-size) *2) calc(var(--cell-size) *2);
 		}
         `;
 	}
@@ -23,14 +38,31 @@ class ChessBoard extends HTMLElement {
 		this.render();
 	}
 
+	renderCells() {
+		const cells = [];
+		for (let y = 0; y < 8; y++) {
+			for (let x = 0; x < 8; x++) {
+				cells.push(this.renderCell(y, x));
+			}
+		}
+		return cells.join("");
+	}
+	renderCell(y, x) {
+		const col = String.fromCharCode(65 + x);
+		const row = 9 - (y + 1);
+		return /*html*/ `
+		<chess-cell x="${col}"y="${row}"></chess-cell>
+		`;
+	}
+
 	render() {
 		this.shadowRoot.innerHTML = /*html*/ `
 		<style>${ChessBoard.styles}</style>
-		<div class="board"></div>
+		<div class="board">
+		${this.renderCells()}
+		</div>
 		`;
 	}
 }
-
-console.log("chess");
 
 customElements.define("chess-board", ChessBoard);
