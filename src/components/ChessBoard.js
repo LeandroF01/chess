a14741c5cafa4d03000f85fcb9e139e4ad6ae60b
Leandro Farias <104572164+LeandroF01@users.noreply.github.com>
2023-05-03 20:20:58 -0300

    Add styles and genFake for position piece

--- src/components/ChessBoard.js
+++ src/components/ChessBoard.js
@@ -11,18 +11,52 @@ class ChessBoard extends HTMLElement {
         :host{
             --pleace-size: 54px;
             --cell-size: 72px;
-			--board-size: 576px;
+			--board-size: calc(var(--cell-size) * 8);
 
 			--color-odd: #eed2aa;
 			--color-even: #90502f;
+			--color-frame: #62351f; 
+
+			--border-style: 0;
         }
+		.frame{
+			display: grid;
+        grid-template-areas: "top top top"
+                              "left board right"
+                              "bottom bottom bottom";
+        justify-content: center;
+        align-items: center;
+        width: calc(var(--cell-size) * 10);
+        height: calc(var(--cell-size) * 10);
+        background: var(--color-frame);
+        font-family: Montserrat, sans-serif;
+        color: #eee; 
+		}
+
+		
+		.row{ display: flex;}
+		.top { grid-area: top; }
+		.bottom { grid-area: bottom; }
+		.left { grid-area: left; }
+		.right { grid-area: right; }
+		.board { grid-area: board; }
+
+		.fake {
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			width: var(--cell-size);
+			height: var(--cell-size);
+			border: 1px solid #fff;
+			box-sizing: border-box;
+		}
 		.board{
 			display: flex;
 			flex-wrap: wrap;
 			width: var(--board-size);
 			height: var(--board-size);
 			background: var(--color-even);
-
+			border: var(--border-style);
 			background: conic-gradient(
 				var(--color-even) 90deg,
 				var(--color-odd) 90deg 180deg,
@@ -31,6 +65,8 @@ class ChessBoard extends HTMLElement {
 			);
 			background-size: calc(var(--cell-size) *2) calc(var(--cell-size) *2);
 		}
+
+		
         `;
 	}
 
@@ -47,6 +83,7 @@ class ChessBoard extends HTMLElement {
 		}
 		return cells.join("");
 	}
+
 	renderCell(y, x) {
 		const col = String.fromCharCode(65 + x);
 		const row = 9 - (y + 1);
@@ -55,13 +92,33 @@ class ChessBoard extends HTMLElement {
 		`;
 	}
 
+	genFakeCells(n) {
+		const texts = (n === 8 ? "87654321" : " abcdefgh ").split("");
+		return texts
+			.map((text) => /* html */ `<div class="fake">${text}</div>`)
+			.join("");
+	}
+
 	render() {
 		this.shadowRoot.innerHTML = /*html*/ `
 		<style>${ChessBoard.styles}</style>
-		<div class="board">
-		${this.renderCells()}
-		</div>
-		`;
+		<div class="frame">
+			<div class="row top">
+			${this.genFakeCells(10)}
+			</div>
+				<div class="col left">
+				${this.genFakeCells(8)}
+				</div>
+					<div class="board">
+						${this.renderCells()}
+					</div>
+				<div class="col right">
+				${this.genFakeCells(8)}
+				</div>
+			<div class="row bottom">
+			${this.genFakeCells(10)}
+			</div>
+		</div>`;
 	}
 }
 
