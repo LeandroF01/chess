63d9ef80aba2a34aa25acfd1fac8fbb1656f0d6b
Leandro Farias <104572164+LeandroF01@users.noreply.github.com>
2023-05-05 20:52:28 -0300

    Add images of pieces with color and type

--- src/components/ChessPiece.js
+++ src/components/ChessPiece.js
@@ -1,3 +1,12 @@
+const PIECES = {
+	K: "king",
+	Q: "queen",
+	R: "rook",
+	B: "bishop",
+	N: "knight",
+	P: "pawn",
+};
+
 class ChessPiece extends HTMLElement {
 	constructor() {
 		super();
@@ -9,19 +18,29 @@ class ChessPiece extends HTMLElement {
         :host {
             /* Estilos para :host */
         }
+		.piece img {
+		// image-rendering: pixelated;
+		filter: drop-shadow(1px 1px 1px #0006);
+        cursor: pointer;
+		}
         `;
 	}
 
 	connectedCallback() {
 		this.type = this.getAttribute("type");
+		this.color = ["K", "Q", "R", "B", "N", "P"].includes(this.type)
+			? "black"
+			: "white";
 		this.render();
 	}
 
 	render() {
+		const piece = PIECES[this.type.toUpperCase()];
 		this.shadowRoot.innerHTML = /*html*/ `
         <style>${ChessPiece.styles}</style>
         <div class="piece">
-            /* Contenido */
+            <img src="../assets/pieces/${this.color}-${piece}.png" 
+			alt="${this.color}-${piece}" />
         </div>
         `;
 	}
